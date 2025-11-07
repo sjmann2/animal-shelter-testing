4a3421a70ac29260dba67b2405b97de86efc7463
Sid Mann <sidmann@Sids-MacBook-Air.local>
2025-11-07 12:33:50 -0600

    hard code user info

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,7 +2,11 @@ import { Heart, Users, Home, Award } from 'lucide-react';
 import Intercom from '@intercom/messenger-js-sdk';
 
 Intercom({
-  app_id: 'la2b8pe2',
+  app_id: "la2b8pe2",
+  user_id: "1", // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+  name: "Sid Mann", // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
+  email: "sid.mann+shelter+testing@intercom.io", // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
+  created_at: 1762561760, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
 });
 
 export default function AboutPage() {
